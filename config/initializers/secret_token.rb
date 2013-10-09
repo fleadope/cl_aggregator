ec1854d9da7efc731511afeaf28c6d2b57ed6315
Lee Pope <leepope@gmail.com>
2013-10-08 19:01:07 -0700

    rails_apps_composer: initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ClAggregator::Application.config.secret_key_base = '4d92367d0eaa6f9e47489830e651f262fd9b9d68072a5151f9c0101f82797dea0271ef6548d634d956d328a0783336f567e4e5e0e2bdde7fde8dd9e312f823c4'
+ClAggregator::Application.config.secret_key_base = '113b19e2bbc5871076bd476884a6dc245d904a6fc624324f1ede135dd6c4080868aa5f46439829ab406a506700aac28108ea332a38e13ef3f8a0e16b936be46f'
