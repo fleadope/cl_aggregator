e00b01e69636c0e726e9398bf473be1011e4ac91
Lee Pope <leepope@gmail.com>
2013-10-15 12:24:20 -0700

    add an "all" option to zeus

--- custom_plan.rb
+++ custom_plan.rb
@@ -7,6 +7,21 @@ class CustomPlan < Zeus::Rails
         RSpec::Core::Runner.disable_autorun!
         exit RSpec::Core::Runner.run(argv)
     end
+
+    def all_specs(argv = ARGV)
+        append_pattern_and_run("spec/**/*_spec.rb", argv)
+    end
+
+    private 
+    def append_pattern_and_run(glob, argv)
+        pattern = Dir.glob(glob).inject(%w[--pattern]) do |result, file|
+            result << file
+        end
+        argv += pattern
+        spec(argv)
+    end
+
+
 end
 
 Zeus.plan = CustomPlan.new
